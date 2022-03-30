65990b28576107ac05ab781a9aac54bc6267999f
saqqdy <saqqdy@qq.com>
2022-03-30 13:22:21 +0800

    1.0.0

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -6,7 +6,8 @@ import { packages } from '../build/packages'
 
 // execSync('npm run build', { stdio: 'inherit' })
 
-let command = 'npm publish --access public'
+const REGISTRY_URL = 'https://registry.npmjs.org'
+let command = `npm --registry=${REGISTRY_URL} publish --access public`
 
 if (version.includes('beta')) command += ' --tag beta'
 if (version.includes('alpha')) command += ' --tag alpha'

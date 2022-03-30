c7554e5dba62a9ba87fa13518bae262d7228cbf4
saqqdy <saqqdy@qq.com>
2022-03-30 13:28:25 +0800

    1.0.0

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,10 +1,10 @@
 import { execSync } from 'child_process'
-import path from 'path'
+import { join } from 'path'
 import consola from 'consola'
 import { version } from '../package.json'
 import { packages } from '../build/packages'
 
-// execSync('npm run build', { stdio: 'inherit' })
+// execSync('pnpm build', { stdio: 'inherit' })
 
 const REGISTRY_URL = 'https://registry.npmjs.org'
 let command = `npm --registry=${REGISTRY_URL} publish --access public`
@@ -15,7 +15,11 @@ if (version.includes('alpha')) command += ' --tag alpha'
 for (const { name } of packages) {
     execSync(command, {
         stdio: 'inherit',
-        cwd: path.join('packages', name, 'dist')
+        cwd: join('packages', name, 'dist')
     })
     consola.success(`Published @eslint-sets/${name}`)
 }
+execSync(command, {
+    stdio: 'inherit'
+})
+consola.success(`Published @eslint-sets/monorepo`)

560a9348e54b27ca24f172d3f4574de964b1739f
saqqdy <saqqdy@qq.com>
2022-12-20 16:23:25 +0800

    new way to sync

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -1,8 +1,10 @@
 import { execSync } from 'child_process'
 import { packages } from '../build/packages'
 
-const cmd = process.platform === 'win32' ? 'start' : 'open'
+// const cmd = process.platform === 'win32' ? 'start' : 'open'
 
 for (const pkg of packages) {
-    execSync(`${cmd} https://npmmirror.com/sync/@eslint-sets/${pkg.pkgName}`)
+    // execSync(`${cmd} https://npmmirror.com/sync/@eslint-sets/${pkg.pkgName}`)
+    execSync(`curl -X PUT -d "sync_upstream=true" "https://registry-direct.npmmirror.com/@eslint-sets/${pkg.pkgName}/sync"`)
 }
+execSync(`curl -X PUT -d "sync_upstream=true" "https://registry-direct.npmmirror.com/@eslint-sets/monorepo/sync"`)

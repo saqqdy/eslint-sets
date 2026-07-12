4d2bac9fcc533b5a436b9b2e51922bda058c6b74
saqqdy <saqqdy@qq.com>
2026-07-12 14:28:33 +0800

    feat: set base path for VitePress configuration

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
+	base: '/eslint-sets/',
 	title: '@eslint-sets/eslint-config',
 	description: 'Modern ESLint config with flat config support',
 
